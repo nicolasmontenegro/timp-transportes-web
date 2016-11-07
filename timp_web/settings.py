b629458bd1ac8c1315b0dcd2ad9b29eecd923b31
Nicolas Montenegro <niko_nmv@live.cl>
2016-11-07 18:41:59 -0300

    Fixed 'settings.py' for STATIC_ROOT

--- timp_web/settings.py
+++ timp_web/settings.py
@@ -128,11 +128,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
